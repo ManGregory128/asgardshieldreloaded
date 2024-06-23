c4d72950f195d6e9545a36ba3131ed4d4f5aa727
Gregory Manitaras <manitarasg@gmail.com>
2024-06-23 19:30:57 +0300

    Completely transitioned to DataGen, Added JEI as a Dependency to test recipes in-game

--- src/main/java/me/mangregory/asr/data/recipe/provider/NormalCraftingTableRecipeProvider.java
+++ src/main/java/me/mangregory/asr/data/recipe/provider/NormalCraftingTableRecipeProvider.java
@@ -34,7 +34,7 @@ public class NormalCraftingTableRecipeProvider extends MainModRecipeProvider {
 
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.STONE_GIANT_SWORD.get())
                 .define('/', Tags.Items.RODS_WOODEN)
-                .define('#', Tags.Items.STONES)
+                .define('#', ItemTags.STONE_TOOL_MATERIALS)
                 .pattern(" ##")
                 .pattern("###")
                 .pattern("/# ")
@@ -77,5 +77,133 @@ public class NormalCraftingTableRecipeProvider extends MainModRecipeProvider {
                 .pattern("/# ")
                 .unlockedBy("has_item", has(Tags.Items.INGOTS_GOLD))
                 .save(output, "ender_giant_sword");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.WOODEN_SHIELD.get())
+                .define('#', ItemTags.PLANKS)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Tags.Items.LEATHERS))
+                .save(output, "wooden_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.STONE_SHIELD.get())
+                .define('#', ItemTags.STONE_TOOL_MATERIALS)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Tags.Items.LEATHERS))
+                .save(output, "stone_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.SKULL_SHIELD.get())
+                .define('#', ItemTags.STONE_TOOL_MATERIALS)
+                .define('/', Tags.Items.LEATHERS)
+                .define('S', Items.SKELETON_SKULL)
+                .pattern("#S#")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Items.SKELETON_SKULL))
+                .save(output, "skull_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.NETHERQUARTZ_SHIELD.get())
+                .define('#', Tags.Items.GEMS_QUARTZ)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Tags.Items.GEMS_QUARTZ))
+                .save(output, "netherquartz_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.IRON_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_IRON)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Tags.Items.INGOTS_IRON))
+                .save(output, "iron_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.ENDER_SHIELD.get())
+                .define('#', Items.OBSIDIAN)
+                .define('E', Items.ENDER_EYE)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("#E#")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Items.ENDER_EYE))
+                .save(output, "ender_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.DIAMOND_SHIELD.get())
+                .define('#', Tags.Items.GEMS_DIAMOND)
+                .define('/', Tags.Items.LEATHERS)
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(Tags.Items.GEMS_DIAMOND))
+                .save(output, "diamond_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_WOODEN_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.WOODEN_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.WOODEN_SHIELD.get()))
+                .save(output, "gilded_wooden_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_STONE_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.STONE_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.STONE_SHIELD.get()))
+                .save(output, "gilded_stone_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_SKULL_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.SKULL_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.SKULL_SHIELD.get()))
+                .save(output, "gilded_skull_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_NETHERQUARTZ_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.NETHERQUARTZ_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.NETHERQUARTZ_SHIELD.get()))
+                .save(output, "gilded_netherquartz_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_IRON_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.IRON_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.IRON_SHIELD.get()))
+                .save(output, "gilded_iron_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_ENDER_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.ENDER_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.ENDER_SHIELD.get()))
+                .save(output, "gilded_ender_shield");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, ItemInit.GILDED_DIAMOND_SHIELD.get())
+                .define('#', Tags.Items.INGOTS_GOLD)
+                .define('/', ItemInit.DIAMOND_SHIELD.get())
+                .pattern("###")
+                .pattern("#/#")
+                .pattern(" # ")
+                .unlockedBy("has_item", has(ItemInit.DIAMOND_SHIELD.get()))
+                .save(output, "gilded_diamond_shield");
     }
 }
