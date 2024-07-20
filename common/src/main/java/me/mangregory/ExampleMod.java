49f04dfab55b79e94e39ac4be8e2eba11745d934
Gregory Manitaras <manitarasg@gmail.com>
2024-07-20 19:51:12 +0300

    Added Architectury Template

---
+++ common/src/main/java/me/mangregory/ExampleMod.java
@@ -0,0 +1,9 @@
+package me.mangregory;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "asr";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/me/mangregory/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package me.mangregory.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import me.mangregory.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
