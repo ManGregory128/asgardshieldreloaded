eec7a1c6ecf06fb3d22f204bec01394b75ea8203
Gregory Manitaras <manitarasg@gmail.com>
2025-08-31 14:09:22 +0100

    Bump MC to 1.21.8

---
+++ common/src/main/java/me/mangregory/AsgardShieldReloaded.java
@@ -0,0 +1,14 @@
+package me.mangregory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class AsgardShieldReloaded {
+    public static final String MOD_ID = "asr";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+        LOGGER.info("Asgard's Shield Reloaded initialized");
+    }
+}
---
+++ neoforge/src/main/java/me/mangregory/neoforge/AsgardShieldReloadedNeoForge.java
@@ -0,0 +1,13 @@
+package me.mangregory.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import me.mangregory.AsgardShieldReloaded;
+
+@Mod(AsgardShieldReloaded.MOD_ID)
+public final class AsgardShieldReloadedNeoForge {
+    public AsgardShieldReloadedNeoForge() {
+        // Run our common setup.
+        AsgardShieldReloaded.init();
+    }
+}
