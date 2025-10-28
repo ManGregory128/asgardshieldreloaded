cd85d8affe6a52e89a8b1027983a3b0f82f85d6f
Me <135455255+IcarussOne@users.noreply.github.com>
2025-10-28 01:38:26 -0500

    Start dyeable shields

--- src/main/java/com/mangregory/asgardshieldreloaded/AsgardShieldReloaded.java
+++ src/main/java/com/mangregory/asgardshieldreloaded/AsgardShieldReloaded.java
@@ -1,9 +1,17 @@
 package com.mangregory.asgardshieldreloaded;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.mangregory.asgardshieldreloaded.compat.CompatHandler;
+import com.mangregory.asgardshieldreloaded.compat.harkenscythe.init.HSModItems;
+import com.mangregory.asgardshieldreloaded.init.ModItems;
+import com.mangregory.asgardshieldreloaded.items.IDyeable;
 
 @Mod(modid = AsgardShieldReloaded.MOD_ID, name = AsgardShieldReloaded.NAME, version = AsgardShieldReloaded.VERSION, acceptedMinecraftVersions = AsgardShieldReloaded.ACCEPTED_VERSIONS, dependencies = AsgardShieldReloaded.DEPENDENCIES)
 public class AsgardShieldReloaded
@@ -19,5 +27,46 @@ public class AsgardShieldReloaded
     public void init(FMLPreInitializationEvent event)
     {
         CompatHandler.init();
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Mod.EventHandler
+    public void initClient(FMLPreInitializationEvent event)
+    {
+        IItemColor itemColorHandler = (stack, tintIndex) ->
+        {
+            if (tintIndex == 1 && stack.getItem() instanceof IDyeable)
+            {
+                return ((IDyeable) stack.getItem()).getDyedColor(stack);
+            } else
+            {
+                return -1;
+            }
+        };
+        
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.BLESSED_GOLDEN_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.DIAMOND_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.ENDER_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_DIAMOND_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_ENDER_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_IRON_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_NETHERQUARTZ_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_PATCHWORK_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_SKULL_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_STONE_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.GILDED_WOODEN_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.IRON_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.NETHERQUARTZ_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.PATCHWORK_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.SKULL_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.STONE_SHIELD);
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, ModItems.WOODEN_SHIELD);
+
+        if (Loader.isModLoaded("harkenscythe")) {
+            Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, HSModItems.BIOMASS_SHIELD);
+            Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, HSModItems.GILDED_BIOMASS_SHIELD);
+            Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, HSModItems.GILDED_LIVINGMETAL_SHIELD);
+            Minecraft.getMinecraft().getItemColors().registerItemColorHandler(itemColorHandler, HSModItems.LIVINGMETAL_SHIELD);
+        }
     }
 }
