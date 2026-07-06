1072822dd87d823f873d8fa5f546f73159d15f5e
Gregory Manitaras <52697798+ManGregory128@users.noreply.github.com>
2026-07-06 01:08:02 +0100

    Fix: Tag Swords to Allow Enchantments

--- fabric/src/main/java/me/mangregory/asr/fabric/client/AsgardShieldReloadedDataGen.java
+++ fabric/src/main/java/me/mangregory/asr/fabric/client/AsgardShieldReloadedDataGen.java
@@ -2,6 +2,7 @@ package me.mangregory.asr.fabric.client;
 
 import me.mangregory.asr.fabric.client.datagen.EnUsLangProvider;
 import me.mangregory.asr.fabric.client.datagen.ItemDefinitionsProvider;
+import me.mangregory.asr.fabric.client.datagen.ItemTagsProvider;
 import me.mangregory.asr.fabric.client.datagen.ModelProvider;
 import me.mangregory.asr.fabric.client.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -14,6 +15,7 @@ public class AsgardShieldReloadedDataGen implements DataGeneratorEntrypoint {
         var pack = generator.createPack();
         pack.addProvider(EnUsLangProvider::new);
         pack.addProvider(ModelProvider::new);
+        pack.addProvider(ItemTagsProvider::new);
         pack.addProvider((FabricDataGenerator.Pack.Factory<ItemDefinitionsProvider>) ItemDefinitionsProvider::new);
         pack.addProvider(ModRecipeProvider::new);
     }
---
+++ fabric/src/main/java/me/mangregory/asr/fabric/client/datagen/ItemTagsProvider.java
@@ -0,0 +1,43 @@
+package me.mangregory.asr.fabric.client.datagen;
+
+import me.mangregory.asr.items.init.AsgardShieldItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+import org.jspecify.annotations.NonNull;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+public class ItemTagsProvider extends FabricTagsProvider.ItemTagsProvider {
+
+    public ItemTagsProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.@NonNull Provider provider) {
+        add(ItemTags.SWORDS, AsgardShieldItems.DIAMOND_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.GOLDEN_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.IRON_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.STONE_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.WOODEN_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.NETHERQUARTZ_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.ENDER_GIANT_SWORD.get());
+        add(ItemTags.SWORDS, AsgardShieldItems.SKULL_GIANT_SWORD.get());
+    }
+
+    private void add(TagKey<Item> tag, ItemLike... suppliers) {
+        tag(tag).addAll(Stream.of(suppliers)
+                .map(ItemLike::asItem)
+                .map(Item::builtInRegistryHolder)
+                .map(Holder.Reference::key)
+                .toList());
+    }
+}
